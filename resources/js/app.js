bdc301f2103e27310f52757d87886beed04ec90d
Luminaire1337 <16178449+Luminaire1337@users.noreply.github.com>
2025-07-04 21:51:04 +0200

    Replace tailwindcss with sass, add essential scss setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import "./bootstrap";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Smooth scroll for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let target = document.querySelector(this.getAttribute("href"));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: "smooth",
+                });
+            }
+        });
+    });
+});
